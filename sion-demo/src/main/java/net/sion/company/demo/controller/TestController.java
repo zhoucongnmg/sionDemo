60b16db84c853419a75f2f148f19f053736a6ee5
liuqiang <liuqiang@163>
2015-06-03 17:08:54 +0800

    commit.

--- sion-demo/src/main/java/net/sion/company/demo/controller/TestController.java
+++ sion-demo/src/main/java/net/sion/company/demo/controller/TestController.java
@@ -25,6 +25,7 @@ public class TestController {
 	
 	@RequestMapping(value = "/user/save")
 	public @ResponseBody Response save(HttpSession session, @RequestBody User user) {
+		System.out.println("111");
 		System.out.println(user);
 		user.setId(StringUtils.isBlank(user.getId()) ? null : user.getId());
 		mongoOps.save(user);
